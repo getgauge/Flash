fd1d0417b82e4909c72c5497853ac07c90fe0601
BugDiver <vinaysh@thoughtworks.com>
2019-12-05 14:42:29 +0530

    Using gRPC for communication with gauge.

--- main.go
+++ main.go
@@ -19,24 +19,32 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net"
 
 	"github.com/getgauge/flash/event"
+	gm "github.com/getgauge/flash/gauge_messages"
 	"github.com/getgauge/flash/listener"
+	"google.golang.org/grpc"
 )
 
-const (
-	gaugeHost    = "localhost"
-	gaugePortEnv = "plugin_connection_port"
-)
+func startAPI(e chan event.Event) {
+	address, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic("failed to start server.")
+	}
+	l, err := net.ListenTCP("tcp", address)
+	if err != nil {
+		panic("failed to start server.")
+	}
+	server := grpc.NewServer(grpc.MaxRecvMsgSize(1024 * 1024 * 1024 * 10))
+	h := listener.NewHandler(server, e)
+	gm.RegisterReporterServer(server, h)
+	fmt.Printf("Listening on port:%d\n", l.Addr().(*net.TCPAddr).Port)
+	server.Serve(l)
+}
 
 func main() {
 	e := make(chan event.Event)
-	a, err := listener.NewApiListener(gaugeHost, os.Getenv(gaugePortEnv), e)
-	if err != nil {
-		fmt.Println("Could not create the gauge listener")
-		os.Exit(1)
-	}
-	go a.Start()
+	go startAPI(e)
 	listener.NewHttpListener(e).Start()
 }
